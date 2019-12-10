c7f09b38e280fe6bc81e33b37cdb62921d62b9d7
Meagan DiPolo <meagan.dipolo@gmail.com>
2019-12-09 22:37:52 -0500

    finished

--- js/scripts.js
+++ js/scripts.js
@@ -2,24 +2,25 @@ const modal = document.querySelector('#modal')
 const button = document.querySelector('#btn')
 const close = document.querySelector('#close')
 const submit = document.getElementById('submit');
+const video = document.getElementsByClassName('videos');
 
 const makeitbetter = document.querySelector('#makeitbetter');
 const up = document.querySelector('#up');
 const lingerie = document.querySelector('#lingerie');
 const someone = document.querySelector('#someone');
-const froot = document.querySelector('#froot');
+const lies = document.querySelector('#lies');
 const perm = document.querySelector('#perm');
 const girls = document.querySelector('#girls');
 const four = document.querySelector('#four');
-const woman = document.querySelector('#woman');
+const heart = document.querySelector('#heart');
 const baddest = document.querySelector('#baddest');
 const west = document.querySelector('#west');
-const nightmare = document.querySelector('#nightmare');
+const smoke = document.querySelector('#smoke');
 const devil = document.querySelector('#devil');
 const twoweeks = document.querySelector('#twoweeks');
-const sayso = document.querySelector('#sayso');
-const bonfire = document.querySelector('#bonfire');
-const realer = document.querySelector('#realer');
+const tyrant = document.querySelector('#tyrant');
+const time = document.querySelector('#time');
+const juicy = document.querySelector('#juicy');
 
 const pop = document.getElementById('pop');
 const rock = document.getElementById('rock');
@@ -31,23 +32,23 @@ const male = document.getElementById('male');
 const female = document.getElementById('female');
 
 let m1 = "You got Make It Better by Anderson .Paak ft. Smokey Robinson!";
-let m2 = "You got Up All Night by Khalid!"
+let m2 = "You got Someone New by Hozier!"
 let m3 = "You got Lingerie by Lizzo!"
-let m4 = "You got Someone New by Hozier!"
-let m5 = "You got Froot by Marina & the Diamonds!"
+let m4 = "You got Up All Night by Khalid!"
+let m5 = "You got Lies by Marina & the Diamonds!"
 let m6 = "You got Perm by Bruno Mars!"
 let m7 = "You got Me & My Girls by Selena Gomez!"
 let m8 = "You got Four out of Five by Arctic Monkeys!"
-let m9 = "You got WOMAN by Karen O and Danger Mouse!"
+let m9 = "You got Hate to See Your Heart Break by Paramore!"
 let m10 = "You got The Baddest Man Alive by The Black Keys!"
-let m11 = "You got Grudges by Paramore!"
+let m11 = "You got Woman by Karen O and Danger Mouse!"
 let m12 = "You got West Coast Smoker by Fall Out Boy!"
-let m13 = "You got Nightmare by PVRIS!"
+let m13 = "You got Smoke by PVRIS!"
 let m14 = "You got Devil in a New Dress by Kanye West!"
 let m15 = "You got Two Weeks by FKA Twigs!"
-let m16 = "You got Say So by Doja Cat!"
-let m17 = "You got Bonfire by Childish Gambino!"
-let m18 = "You got Realer by Megan Thee Stallion!"
+let m16 = "You got Tyrant by Kali Uchis ft. Jorja Smith!"
+let m17 = "You got I Ain't Got Time by Tyler, the Creator!"
+let m18 = "You got Juicy by Doja Cat!"
 let m19 = "Please select an answer for all three questions."
 
 function hideAllSongs() {
@@ -55,20 +56,20 @@ function hideAllSongs() {
   up.style.display = 'none';
   lingerie.style.display = 'none';
   someone.style.display = 'none';
-  froot.style.display = 'none';
+  lies.style.display = 'none';
   perm.style.display = 'none';
   girls.style.display = 'none';
   four.style.display = 'none';
-  woman.style.display = 'none';
+  heart.style.display = 'none';
   baddest.style.display = 'none';
-  grudges.style.display = 'none';
+  woman.style.display = 'none';
   west.style.display = 'none';
-  nightmare.style.display = 'none';
+  smoke.style.display = 'none';
   devil.style.display = 'none';
   twoweeks.style.display = 'none';
-  sayso.style.display = 'none';
-  bonfire.style.display = 'none';
-  realer.style.display = 'none';
+  tyrant.style.display = 'none';
+  time.style.display = 'none';
+  juicy.style.display = 'none';
 }
 
 hideAllSongs();
@@ -84,7 +85,7 @@ function showSong () {
       hideAllSongs();
       message = m2;
       document.querySelector(".textresult").textContent = message;
-      up.style.display = 'block';
+      someone.style.display = 'block';
     }
     else if (pop.checked === true && slow.checked === true && female.checked === true) {
       hideAllSongs();
@@ -96,13 +97,13 @@ function showSong () {
       hideAllSongs();
       message = m4;
       document.querySelector(".textresult").textContent = message;
-      someone.style.display = 'block';
+      up.style.display = 'block';
     }
     else if (pop.checked === true && medium.checked === true && female.checked === true) {
       hideAllSongs();
       message = m5;
       document.querySelector(".textresult").textContent = message;
-      froot.style.display = 'block';
+      lies.style.display = 'block';
     }
     else if (pop.checked === true && fast.checked === true && male.checked === true) {
       hideAllSongs();
@@ -126,7 +127,7 @@ function showSong () {
       hideAllSongs();
       message = m9;
       document.querySelector(".textresult").textContent = message;
-      woman.style.display = 'block';
+      heart.style.display = 'block';
     }
     else if (rock.checked === true && medium.checked === true && male.checked === true) {
       hideAllSongs();
@@ -138,7 +139,7 @@ function showSong () {
       hideAllSongs();
       message = m11;
       document.querySelector(".textresult").textContent = message;
-      grudges.style.display = 'block';
+      woman.style.display = 'block';
     }
     else if (rock.checked === true && fast.checked === true && male.checked === true) {
       hideAllSongs();
@@ -150,7 +151,7 @@ function showSong () {
       hideAllSongs();
       message = m13;
       document.querySelector(".textresult").textContent = message;
-      nightmare.style.display = 'block';
+      smoke.style.display = 'block';
     }
     else if (hiphop.checked === true && slow.checked === true && male.checked === true) {
       hideAllSongs();
@@ -168,19 +169,19 @@ function showSong () {
       hideAllSongs();
       message = m16;
       document.querySelector(".textresult").textContent = message;
-      sayso.style.display = 'block';
+      tyrant.style.display = 'block';
     }
     else if (hiphop.checked === true && fast.checked === true && male.checked === true) {
       hideAllSongs();
       message = m17;
       document.querySelector(".textresult").textContent = message;
-      bonfire.style.display = 'block';
+      time.style.display = 'block';
     }
     else if (hiphop.checked === true && fast.checked === true && female.checked === true) {
       hideAllSongs();
       message = m18;
       document.querySelector(".textresult").textContent = message;
-      realer.style.display = 'block';
+      juicy.style.display = 'block';
     }
     else {
       hideAllSongs();
@@ -193,32 +194,10 @@ button.onclick = () => {
   showSong();
 }
 
-$('#close').click(function(){
-  $('.videos').each(function(){
-    $(this).stopVideo();
+//Adapted code taken from Bartekkustra's comment on this forum: https://forum.webflow.com/t/how-to-pause-and-stop-a-video-in-a-modal-on-close/2366/6
+$('#modal').on('hidden.bs.modal', function (e) {
+    e.preventDefault();
+    $('.videos').children('iframe').attr('src', '');
+// End of adapted code
+    location.reload();
   });
-});
-//
-// var stopAllYouTubeVideos = () => {
-//   var iframes = document.querySelectorAll('iframe');
-//   Array.prototype.forEach.call(iframes, iframe => {
-//     iframe.contentWindow.postMessage(JSON.stringify({ event: 'command',
-//   func: 'stopVideo' }), '*');
-//  });
-// }
-
-// function pauseAllVideos() {
-//     $('.close').click(function(){
-//          $('iframe').attr('src', $('iframe').attr('src'));
-//      });
-// }
-
-// $('a.stop-video').click(function(){
-// 	$('.youtube-video')[0].contentWindow.postMessage('{"event":"command","func":"' + 'stopVideo' + '","args":""}', '*');
-// });
-//
-//
-// $('#modal').on('hidden.bs.modal', function () {
-//     stopVideo();
-// });
-//   //
